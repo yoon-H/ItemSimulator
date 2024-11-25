314c555b430b0186fd2606e2c822eee523e17812
yoon-H <ucando0320@gmail.com>
2024-11-25 16:56:26 +0900

    [INIT] init project

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+const PORT = 3018;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+    console.log(PORT, '포트로 서버가 열렸어요!');
+});
