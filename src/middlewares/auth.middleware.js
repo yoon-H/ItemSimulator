7f69663dfba9554aae1eaa7d4655910d2f719e46
yoon-H <ucando0320@gmail.com>
2024-11-26 21:32:31 +0900

    [ADD] add auth.middleward.js

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,50 @@
+// src/middlewares/auth.middleware.js
+
+import jwt from "jsonwebtoken";
+import { prisma } from "../utils/prisma/index.js";
+
+export default async function (req, res, next) {
+    try {
+        const { authorization } = req.header;
+        if (!authorization) throw new Error("토큰이 존재하지 않습니다.");
+
+        const [tokenType, token] = authorization.split(" ");
+
+        if (tokenType !== "Bearer")
+            throw new Error("토큰 타입이 일치하지 않습니다.");
+
+        const decodedToken = jwt.verify(token, process.env["ACCESS_TOKEN_SECRET_KEY"]);
+        const userId = decodedToken.userId;
+
+        const user = await prisma.users.findFirst({
+            where: { id: userId },
+        });
+        
+        if (!user) {
+            throw new Error("토큰 사용자가 존재하지 않습니다.");
+        }
+
+        // req.user에 사용자 정보를 저장합니다.
+        req.user = user;
+
+        next();
+    } catch (error) {
+        // 토큰이 만료되었거나, 조작되었을 때, 에러 메시지를 다르게 출력합니다.
+        switch (error.name) {
+            case "TokenExpiredError":
+                return res
+                    .status(401)
+                    .json({ message: "토큰이 만료되었습니다." });
+            case "JsonWebTokenError":
+                return res
+                    .status(401)
+                    .json({ message: "토큰이 조작되었습니다." });
+            default:
+                return res
+                    .status(401)
+                    .json({
+                        message: error.message ?? "비정상적인 요청입니다.",
+                    });
+        }
+    }
+}
