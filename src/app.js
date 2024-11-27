88c7391bfc363816a7bd807e2b115cdcc7514a2c
yoon-H <ucando0320@gmail.com>
2024-11-27 12:24:58 +0900

    [FIX] catch joi error

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import UsersRouter from './routes/users.router.js';
+import CharacterRouter from './routes/characters.router.js';
 
 const app = express();
 const PORT = 3018;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/users', [UsersRouter]);
+app.use('/api', [UsersRouter, CharacterRouter]);
 
 app.listen(PORT, () => {
     console.log(PORT, '포트로 서버가 열렸어요!');
