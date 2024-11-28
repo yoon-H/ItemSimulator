a90d332639183e2b999b137225d242bcaddbc79c
yoon-H <ucando0320@gmail.com>
2024-11-28 23:14:48 +0900

    [FIX] fix codes because slot added

--- src/routes/items.router.js
+++ src/routes/items.router.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { prisma } from "../utils/prisma/index.js";
-//import authMiddleware from "../middlewares/auth.middleware.js";
 import Joi from "joi";
 
 const router = express.Router();
 
 const status = new Set(["health", "power"]);
+const slotEnum = new Set(["ARMOR", "WEAPON", "RING", "AMULET"])
 
 const numberSchema = Joi.number().integer().strict().required();
 const stringSchema = Joi.string().required().strict();
@@ -13,6 +13,7 @@ const stringSchema = Joi.string().required().strict();
 // #region 아이템 생성
 router.post("/items", async (req, res, next) => {
     const { itemCode, itemName, itemStat, itemPrice } = req.body;
+    let {itemSlot} = req.body;
 
     // #region 아이템 코드 검사
     try {
@@ -65,9 +66,7 @@ router.post("/items", async (req, res, next) => {
                     message: "아이템 스탯 타입을 형식에 맞게 작성해주세요.",
                 });
 
-                const result = await numberSchema.validateAsync(
-                    itemStat[key],
-                );
+            const result = await numberSchema.validateAsync(itemStat[key]);
         }
     } catch (error) {
         if (error.name === "ValidationError")
@@ -75,6 +74,23 @@ router.post("/items", async (req, res, next) => {
                 .status(400)
                 .json({ message: "아이템 스탯 값을 숫자로 입력해주세요." });
     }
+    // #endregion
+
+    // #region 아이템 슬롯 검사
+    if(!itemSlot) return res.status(400).json({message : "슬롯을 입력해주세요."});
+
+    try {
+        await stringSchema.validateAsync(itemSlot);
+    } catch (error) {
+        if (error.name === "ValidationError")
+            return res
+                .status(400)
+                .json({ message: "아이템 슬롯을 문자열로 입력해주세요." });
+    }
+
+    itemSlot = itemSlot.toUpperCase();
+
+    if(!slotEnum.has(itemSlot)) return res.status(400).json({message : "슬롯 enum 중에 하나를 적어주세요."});
     // #endregion
 
     // #region 아이템 가격 검사
@@ -93,6 +109,7 @@ router.post("/items", async (req, res, next) => {
             itemCode,
             name: itemName,
             stat: itemStat,
+            slot : itemSlot,
             price: itemPrice,
         },
     });
@@ -139,7 +156,8 @@ router.patch("/items/:itemCode", async (req, res, next) => {
         },
     });
 
-    if(isExistName) return res.status(400).json({message : "중복되는 이름입니다."});
+    if (isExistName)
+        return res.status(400).json({ message: "중복되는 이름입니다." });
 
     // #endregion
 
@@ -175,45 +193,50 @@ router.patch("/items/:itemCode", async (req, res, next) => {
         },
     });
 
-    return res.status(200).json({message : "성공적으로 수정되었습니다.", updateItem});
+    return res
+        .status(200)
+        .json({ message: "성공적으로 수정되었습니다.", updateItem });
 });
 // #endregion
 
 // #region 아이템 목록 조회
-router.get('/items', async(req,res,next) => {
+router.get("/items", async (req, res, next) => {
     const items = await prisma.items.findMany({
         select: {
             itemCode: true,
             name: true,
             price: true,
-          },
+        },
     });
 
     return res.status(200).json(items);
-})
+});
 // #endregion
 
 // #region 아이템 상세 조회
-router.get('/items/:itemCode', async (req, res, next) => {
-    const {itemCode} = req.params;
+router.get("/items/:itemCode", async (req, res, next) => {
+    const { itemCode } = req.params;
 
-    if(!itemCode) return res.status(400).json({message : "아이템 코드를 입력해주세요."});
+    if (!itemCode)
+        return res.status(400).json({ message: "아이템 코드를 입력해주세요." });
 
     const item = await prisma.items.findFirst({
-        where : {
-            itemCode : +itemCode,
+        where: {
+            itemCode: +itemCode,
         },
-        select : {
-            itemCode : true,
-            name : true,
-            stat : true,
-            price : true
-        }
-    })
+        select: {
+            itemCode: true,
+            name: true,
+            stat: true,
+            slot : true,
+            price: true,
+        },
+    });
 
-    if(!item) return res.status(400).json({message : "해당하는 아이템이 없습니다."});
+    if (!item)
+        return res.status(400).json({ message: "해당하는 아이템이 없습니다." });
 
     return res.status(200).json(item);
-})
+});
 
 export default router;
