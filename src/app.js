0248195a353314b13481a39653bb7c1647b879ae
yoon-H <ucando0320@gmail.com>
2024-11-28 15:54:46 +0900

    [UPDATE] add ShopRouter

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const PORT = 3018;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [UsersRouter, CharacterRouter, ItemRouter]);
+app.use('/api', [UsersRouter, CharacterRouter, ItemRouter, ShopRouter]);
 
 app.listen(PORT, () => {
     console.log(PORT, '포트로 서버가 열렸어요!');
